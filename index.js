35f415bd5472ac9d062f6dfbe1c272bddbf020b9
Powsihan <powsipowsihan07@gmail.com>
2024-05-11 14:15:32 +0530

    backend user register function added

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from "./config/db.js";
+import cookieParser from 'cookie-parser';
+import {errorHandler, notFound} from "./middleware/errorMiddleware.js";
+
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+connectDB();
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(express.urlencoded({extended: true}))
+
+app.use('/api/users',userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
