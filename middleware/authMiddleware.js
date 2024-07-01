ce90550c489c5d81179d6d8dce2dd9041b98e5e4
Powsihan <powsipowsihan07@gmail.com>
2024-07-01 10:54:05 +0530

    middleware chnaged

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,18 @@
 import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
-import User from "../models/userModal.js";
+import User from "../modals/userModal.js";
 
 const protect = asyncHandler(async (req, res, next) => {
-    let token = req.cookies.jwt;
-    console.log(token);
-    // console.log(token);
+    const authHeader = req?.headers?.authorization;
+    if (!authHeader || !authHeader?.startsWith("Bearer")) {
+        res.status(401);
+        throw new Error("Not authorized, no token");
+    }
+    const token = authHeader?.split(" ")[1];
+
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            console.log(decoded);
             req.user = await User.findById(decoded.userId).select("-password");
             next();
 
