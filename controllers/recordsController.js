efe56c0bcf5234c5dd4940f374ea4cc1d5b12be3
Powsihan <powsipowsihan07@gmail.com>
2024-07-29 20:19:58 +0530

    vehicle record section done

--- controllers/recordsController.js
+++ controllers/recordsController.js
@@ -13,6 +13,25 @@ const getAllRecords = asyncHandler(async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+const getAllRecordsByID = asyncHandler(async (req, res) => {
+  try {
+    let _id = req.params.id;
+
+    const records = await Records.findById(_id).populate("vehicleId");
+
+    if (!records) {
+      return res
+        .status(404)
+        .json({ message: "No records found." });
+    }
+
+    res.status(200).json(records);
+  } catch (err) {
+    console.error("Failed to fetch records by customer ID from MongoDB:", err);
+    res.status(500).json({ message: err.message });
+  }
+});
+
 
 const getRecordByCustomerID = asyncHandler(async (req, res) => {
   try {
@@ -33,4 +52,28 @@ const getRecordByCustomerID = asyncHandler(async (req, res) => {
   }
 });
 
-export { getAllRecords, getRecordByCustomerID };
+const updateRecordHistory = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const { content, details, documents } = req.body;
+  
+    const record = await Records.findById(id);
+  
+    if (record) {
+      record.recordhistory.push({
+        content: content || "",
+        details: details || "",
+        documents: documents || [],
+      });
+  
+      const updatedRecord = await record.save();
+      res.status(200).json({
+        data: updatedRecord,
+        message: "Record history updated successfully",
+      });
+    } else {
+      res.status(404);
+      throw new Error("Record not found");
+    }
+  });
+
+export { getAllRecords, getRecordByCustomerID, getAllRecordsByID,updateRecordHistory };
--- routes/recordsRoutes.js
+++ routes/recordsRoutes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import {
   getAllRecords,
+  getAllRecordsByID,
   getRecordByCustomerID,
+  updateRecordHistory,
 } from "../controllers/recordsController.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllRecords);
-router.route("/:customerId").get(getRecordByCustomerID);
+router.route("/customer/:customerId").get(getRecordByCustomerID);
+router.route("/:id").get(getAllRecordsByID);
+router.route('/addrecord/:id').put(updateRecordHistory);
 
 export default router;
